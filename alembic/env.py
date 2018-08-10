81feeda40c1aab5ab0ebb1d82fa2711a22cf8ff4
discopatrick <git@discopatrick.me>
2018-08-10 11:41:02 +0100

    Move models into backend module

--- alembic/env.py
+++ alembic/env.py
@@ -13,7 +13,7 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from models import Base
+from backend.models import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
