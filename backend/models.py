d62d4278291c9be9979d658753877d62956c0748
discopatrick <git@discopatrick.me>
2018-08-10 14:10:38 +0100

    Move SQLAlchemy declarative base to a database module

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,7 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+from backend.database import Base
 
 
 class Address(Base):
