a3965e9bbf9c345b46e37c815051116b30bbb4e1
discopatrick <git@discopatrick.me>
2018-08-10 15:12:21 +0100

    Enable access to models from views

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 
+from backend.models import User
+
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     @app.route('/')
     def hello():
-        return "Hello World!"
+        user_list = ''
+        for user in User.query.all():
+            user_list += user.fullname + '\n'
+        return "Hello World!\n" + user_list
 
     return app
--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,12 @@
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+engine = create_engine('mysql://root:password@db/foo')
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
 
 Base = declarative_base()
+Base.query = db_session.query_property()
